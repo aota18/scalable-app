acf074b71344a0bfd7f9c9f541b9db7179d8b363
swseo <59370357+aota18@users.noreply.github.com>
2023-09-12 16:48:47 -0700

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,16 @@
+# Node.js
+
+It is a runtime engine that allows javascript to be excuted on the server side. It's a cross-platform, open-source runtime environment that operates using the V8 Javascript engine.
+
+## Key Features
+
+- Event-driven architecture
+- Non-blocking I/O
+- Performance and speed
+- Highly scalable
+- Promise and async-await
+
+- Event driven architecture: it allows to manage different requests at the same time and execute a function depending on the specific event it recieves. This makes Node.js an excellent choice for developers who need to build applications that can handle multiple tasks simultaneously.
+  Also, this feature makes it the perfect choice for applications that need to run in real time, like chat applications, multiplayer games, etc.
+
+- Non blocking I/O: Node.js is built on a non-blocking I/O model, which enables it to efficiently handle multiple requests in parallel. 
